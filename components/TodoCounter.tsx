384491ed8c66662f7b84c9d3b87668a48bbc93e2
Diego H <diegohermida94@gmail.com>
2022-10-17 16:57:24 -0300

    first commit

---
+++ components/TodoCounter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoCounter = () => {
+  return <div>TodoCounter</div>;
+};
+
+export {TodoCounter};
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TodoCounter } from "../components/TodoCounter";
+
+const Home = () => {
+  return <div>Home
+    <TodoCounter/>
+  </div>;
+};
+
+export default Home;
